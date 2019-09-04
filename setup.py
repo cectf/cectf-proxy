e8755f04f68906ba81aef2d6c56c3ee129d43f72
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-09-03 20:43:31 -0400

    advancing to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cectf-proxy',
-    version='1.0.0',
+    version='1.0.1',
     author="Daniel Chiquito",
     author_email="daniel.chiquito@gmail.com",
     description='Backend for the CECTF',
